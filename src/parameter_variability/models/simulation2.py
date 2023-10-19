2be14adee5b527822106eb286ba425a9f285068e
Matthias König <konigmatt@googlemail.com>
2023-10-19 14:32:18 +0200

    added reference simulation

--- src/parameter_variability/models/simulation2.py
+++ src/parameter_variability/models/simulation2.py
@@ -6,42 +6,76 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 
-r: roadrunner.RoadRunner = roadrunner.RoadRunner("model2.xml")
+def reference_simulation(r: roadrunner.RoadRunner) -> None:
+    """Reference simulation."""
 
-ks = np.linspace(0, 10, num=11)
-cls = np.linspace(0, 10, num=11)
-qs = np.linspace(0, 10, num=11)
+    console.rule("Reference simulation", style="white")
+    f, ax = plt.subplots(nrows=1, ncols=1, figsize=(7, 7), dpi=300)
 
-results = {}
+    # simulation
+    r.resetAll()
+    s = r.simulate(start=0, end=1, steps=400)
+    df: pd.DataFrame = pd.DataFrame(s, columns=s.colnames)
 
-for parameter, par_name in zip([ks, cls, qs], ['k', 'CL', 'Q']):
-    results_par = []
-    for value in parameter:
-        # reset to a clean state
-        r.resetAll()
-        r.setValue(par_name, value)
-        s = r.simulate(start=0, end=10, steps=400)
-        # pretty slow (memory copy)
-        df: pd.DataFrame = pd.DataFrame(s, columns=s.colnames)
-        # console.print(df)
-        results_par.append(df)
+    for sid in ["[y_gut]", "[y_cent]", "[y_peri]"]:
+        ax.plot(df.time, df[sid], label=sid)
 
-    results[par_name] = results_par
+        # ax.legend()
+        ax.set_title("Reference simulation")
+        ax.set_xlabel("time [A.U.]")
+        ax.set_ylabel("concentration [substance_units/compartment_units]")
+
+    ax.legend()
+    ax.grid(True)
+    plt.tight_layout()
+    plt.show()
 
 
-f, axes = plt.subplots(nrows=3, ncols=1, figsize=(7, 13))
-for parameter, ax in zip(results, axes):
-    for sid in ["[y_cent]", "[y_gut]", "[y_peri]"]:
-        for df in results[parameter]:
-            ax.plot(
-                    df.time, df[sid], label=sid,
-                    linestyle="-",
-                    # marker="o",
-                    markeredgecolor="black"
-                )
-    # ax.legend()
-    ax.set_title(parameter)
-    ax.set_xlabel("time [A.U.]")
-    ax.set_ylabel("concentration [substance_units/compartment_units]")
-plt.tight_layout()
-plt.show()
+def parameter_scan(r: roadrunner.RoadRunner) -> None:
+    """Scanning model parameters."""
+    console.rule("Parameter scan", style="white")
+
+    ks = np.linspace(0, 10, num=11)
+    cls = np.linspace(0, 10, num=11)
+    qs = np.linspace(0, 10, num=11)
+
+    results = {}
+
+    for parameter, par_name in zip([ks, cls, qs], ['k', 'CL', 'Q']):
+        results_par = []
+        for value in parameter:
+            # reset to a clean state
+            r.resetAll()
+            r.setValue(par_name, value)
+            s = r.simulate(start=0, end=10, steps=400)
+            # pretty slow (memory copy)
+            df: pd.DataFrame = pd.DataFrame(s, columns=s.colnames)
+            # console.print(df)
+            results_par.append(df)
+
+        results[par_name] = results_par
+
+    f, axes = plt.subplots(nrows=3, ncols=1, figsize=(7, 13), dpi=300)
+    for parameter, ax in zip(results, axes):
+        for sid in ["[y_cent]", "[y_gut]", "[y_peri]"]:
+            for df in results[parameter]:
+                ax.plot(
+                        df.time, df[sid], label=sid,
+                        linestyle="-",
+                        # marker="o",
+                        markeredgecolor="black"
+                    )
+        # ax.legend()
+        ax.set_title(parameter)
+        ax.set_xlabel("time [A.U.]")
+        ax.set_ylabel("concentration [substance_units/compartment_units]")
+    plt.tight_layout()
+    plt.show()
+
+
+if __name__ == "__main__":
+    # load_model
+    r: roadrunner.RoadRunner = roadrunner.RoadRunner("model2.xml")
+    reference_simulation(r)
+    parameter_scan(r)
+
