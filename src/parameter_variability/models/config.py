2b1a5b54e7052712f08c42c5751417424c9f30a6
Antonio Aquiles Alvarez Aguilar <aaguilar@kayak.com>
2023-11-17 19:18:39 +0100

    Toy example project: Sampling finalized. Config script added to deal with the argument parsing

---
+++ src/parameter_variability/models/config.py
@@ -0,0 +1,29 @@
+import argparse
+import os
+import numpy as np
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--mean', type=float, default=np.log(2.5))
+    parser.add_argument('--variance', type=float, default=1)
+    parser.add_argument('--parameter', type=str, default='k')
+    parser.add_argument('-n', type=int, default=1)
+    parser.add_argument('--steps', type=int, default=30)
+    parser.add_argument('--model_path', type=str, default='model2.xml')
+
+    parser.add_argument('--data_dir', type=str, default='data')
+    parser.add_argument('--dir_result_name', type=str, default='sampling')
+
+    args = parser.parse_args()
+
+    setattr(args, 'save_dir', args.data_dir+'/'+args.dir_result_name)
+
+    os.makedirs(args.save_dir, exist_ok=True)
+
+    return args
+
+
+
+
---
+++ src/parameter_variability/models/simulation2_sampling.py
@@ -0,0 +1,110 @@
+"""
+Toy Data Sampler
+
+Using a SBML Model, declare a random distribution to generate solutions to the model as simulations.
+
+
+
+"""
+
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from scipy import stats
+import roadrunner
+import os
+import json
+from config import parse_args
+
+np.random.seed(2001)
+
+
+class Sampler(object):
+    def __init__(self, loc, scale, name, n, steps, model_path, save_dir):
+        self.loc = loc
+        self.scale = scale
+        self.name = name
+        self.n = n
+        self.steps = steps
+        self.model_path = model_path
+        print('-------------------- Sampler Initialized --------------------')
+        self.model = roadrunner.RoadRunner(self.model_path)
+
+        self.true_distribution = None
+        self.thetas = None
+        self.errors_distribution = None
+
+        self.df_res = None
+
+        self.define_distribution()
+        self.draw_theta()
+        self.sample_data()
+        self.save_data(save_dir)
+
+    def define_distribution(self, true_dsn=stats.lognorm, errors_dsn=stats.halfnorm):
+        true_dsn = true_dsn(loc=self.loc, s=self.scale)
+        errors_dsn = errors_dsn()
+        self.true_distribution = true_dsn
+        self.errors_distribution = errors_dsn
+
+        print('\nDistribution defined')
+
+    def draw_theta(self):
+        thetas = self.true_distribution.rvs(size=self.n)
+        self.thetas = thetas
+        print(f'\nThetas drawn: {self.thetas}')
+
+    def sample_data(self):
+        self.model.resetAll()
+        sims = []
+        num_thetas = self.thetas.shape[0]
+
+        for i, theta in enumerate(self.thetas):
+            self.model.setValue(self.name, theta)
+            step_correction = self.steps + 1
+
+            errors = self.errors_distribution.rvs(size=step_correction)
+            sim = self.model.simulate(start=0, end=10, steps=self.steps)
+
+            sim[:, 1:] = sim[:, 1:] + errors.reshape((step_correction, 1))
+            df = pd.DataFrame(sim, columns=sim.colnames)
+            df['patient_id'] = i
+            sims.append(df)
+
+            self.model.resetAll()
+
+        if len(sims) > 1:
+            df_res = pd.concat(sims, axis=1)
+
+        else:
+            df_res = sims[0]
+
+        self.df_res = df_res
+
+    def save_data(self, save_dir):
+        res = self.df_res.to_dict(orient='split')
+        res['thetas'] = self.thetas.tolist()
+        res['n'] = self.n
+
+        with open(save_dir+'/simulation2_samples.json', 'w') as file:
+            json.dump(res, file, indent=4, sort_keys=True)
+
+        print(f'\nResults saved in {save_dir}'
+              f'\n-------------------- Simulation Finished! --------------------')
+        print()
+
+    # TODO: add plotting method to plot and summarize the data
+    def plot(self):
+        pass
+
+
+if __name__ == '__main__':
+    args = parse_args()
+
+    Sampler(loc=args.mean,
+            scale=args.variance,
+            name=args.parameter,
+            n=args.n,
+            steps=args.steps,
+            model_path=args.model_path,
+            save_dir=args.save_dir)
